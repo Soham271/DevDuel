3322582f981eac4aad466f7fad0587b3200ffa45
Soham Patil <145631825+Soham271@users.noreply.github.com>
2025-05-14 13:45:00 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,5 @@
 import app from "./app.js";
-import cloudinary from "cloudinary";
+
 import { fileURLToPath } from "url";
 import path from "path";
 
@@ -7,12 +7,7 @@ import path from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Configure Cloudinary
-cloudinary.v2.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+
 
 // Serve the 'indes.html' file
 app.get("/", (req, res) => {
