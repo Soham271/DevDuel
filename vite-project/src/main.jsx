2aec251f3d1edf343d24aadca2ae24400c452f88
Soham271 <145631825+Soham271@users.noreply.github.com>
2025-05-15 22:56:57 +0530

    Navbar

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -2,12 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { Store } from "./Store/Store";
+import { persistor, store } from "./Store/Store";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Provider store={Store}>
-      <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
